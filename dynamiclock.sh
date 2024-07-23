8291c08aab90a5c40112e61a3ff0aed514f096f9
Logic <telbds@hotmail.com>
2024-07-23 16:06:38 +0800

    更新

--- dynamiclock.sh
+++ dynamiclock.sh
@@ -8,12 +8,17 @@ device="mac address of bluetooth device"
 num_name=$(bluetoothctl info | grep "Name: $name" | wc -l)
 num_device=$(bluetoothctl info | grep "Device $device" | wc -l)
 
-if [ $num_name -eq 0 ] && [ $num_device -eq 0 ]; then
+# 检测是否已经锁定
+lock_status=$(dbus-send --session --dest=org.freedesktop.ScreenSaver --type=method_call --print-reply /org/freedesktop/ScreenSaver org.freedesktop.ScreenSaver.GetActive | grep 'boolean true' |wc -l)
+
+if [ $lock_status -eq 0 ]; then
+  if [ $num_name -eq 0 ] && [ $num_device -eq 0 ]; then
   # qdbus org.kde.screensaver /ScreenSaver Lock
   # qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock
   # dbus-launch qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock
 
-  # 这里应该会锁定所有的会话.
-  loginctl lock-sessions
+    # 这里应该会锁定所有的会话.
+    loginctl lock-sessions
+  fi
 fi
 
