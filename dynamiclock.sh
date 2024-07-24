bf72b3ee8bca5e3af50384fe2c60ea7e93e9bb4f
Logic <telbds@hotmail.com>
2024-07-24 12:20:35 +0800

    优化通知逻辑

--- dynamiclock.sh
+++ dynamiclock.sh
@@ -8,12 +8,19 @@ device="mac address of bluetooth device"
 num_name=$(bluetoothctl info | grep "Name: $name" | wc -l)
 num_device=$(bluetoothctl info | grep "Device $device" | wc -l)
 
+user="which user to watch this message"
+title="未连接指定的蓝牙设备"
+message='请在\"设置\"-\"蓝牙\"中连接用于动态锁定的蓝牙设备.设备将在10秒后自动锁定'
+app="DynamicLock"
+icon="/usr/share/icons/BeautyLine/actions/scalable/lock.svg"
+timeout=5000
+
 # 检测是否已经锁定
 lock_status=$(dbus-send --session --dest=org.freedesktop.ScreenSaver --type=method_call --print-reply /org/freedesktop/ScreenSaver org.freedesktop.ScreenSaver.GetActive | grep 'boolean true' |wc -l)
 
 if [ $lock_status -eq 0 ]; then
   if [ $num_name -eq 0 ] && [ $num_device -eq 0 ]; then
-    notify-send "未连接指定的蓝牙设备" "请在\"设置\"-\"蓝牙\"中连接用于动态锁定的蓝牙设备.设备将在10秒后自动锁定"  -a "DynamicLock" -u normal -i /usr/share/icons/BeautyLine/actions/scalable/lock.svg -t 5000
+    su $user -c "notify-send $title $message -a $app -u critical -i $icon -t $timeout"    
     sleep 10
   # qdbus org.kde.screensaver /ScreenSaver Lock
   # qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock
