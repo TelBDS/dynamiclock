19ab092310a4c5f4757abb9f9ff3fa0dd549ba14
Logic <telbds@hotmail.com>
2024-07-24 11:58:48 +0800

    增加消息提醒

--- dynamiclock.sh
+++ dynamiclock.sh
@@ -13,6 +13,8 @@ lock_status=$(dbus-send --session --dest=org.freedesktop.ScreenSaver --type=meth
 
 if [ $lock_status -eq 0 ]; then
   if [ $num_name -eq 0 ] && [ $num_device -eq 0 ]; then
+    notify-send "未连接指定的蓝牙设备" "请在\"设置\"-\"蓝牙\"中连接用于动态锁定的蓝牙设备.设备将在10秒后自动锁定"  -a "DynamicLock" -u normal -i /usr/share/icons/BeautyLine/actions/scalable/lock.svg -t 5000
+    sleep 10
   # qdbus org.kde.screensaver /ScreenSaver Lock
   # qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock
   # dbus-launch qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock
